ee2e0d57b7a2efba60d863024eacfa7a8ae87dd7
Gabriel Xavier Pereira <gabriel220060@ilum.cnpem.br>
2024-05-27 12:54:49 -0300

    images and edge detection initial algorithm

--- functions.py
+++ functions.py
@@ -1,6 +1,11 @@
-import numpy as np
 import cv2
-from skimage import io
+import numpy as np
+from skimage import measure
+from skimage.color import rgb2gray
+from skimage.feature import canny
+from skimage.filters import threshold_otsu
+from skimage.io import imread, imsave
+from skimage.util import img_as_ubyte
 
 def particles_identification():
     """This function identify and mark the particles in a SEM image.
@@ -25,23 +30,26 @@ def crop_image():
     """
     
 
-def remove_background():
-    """This function remove the background from a SEM image of a particle
+def remove_background(cropped_image):
+    """This function removes the background from a SEM image of a particle
     
     Input:
-        cropped_image: image cropped of a specific particle
+        cropped_image: np.array, image cropped of a specific particle
         
     Output:
-        rmbg_cropped_image: image cropped of a specific particle without background
+        rmbg_cropped_image: np.array, image cropped of a specific particle without background
     """
-    
-def edge_finder():
-    """This function will find the edges of a particle
+
+def edge_finder(rmbg_cropped_image):
+    """This function finds the edges of a particle
     
     Input:
-        rmbg_cropped_image: image cropped of a specific particle without background
+        rmbg_cropped_image: np.array, image cropped of a specific particle without background
         
     Output:
-        particle_edges: image with just the edges of a particle
+        particle_edges: np.array, image with just the edges of a particle
     """
+    gray_image = rgb2gray(rmbg_cropped_image)
+    edges = canny(gray_image)
     
+    return edges
